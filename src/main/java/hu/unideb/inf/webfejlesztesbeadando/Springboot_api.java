df98c6a4e9e2e0b05efafc65088985feb91e849c
Vgabor33 <veressgabi33@gmail.com>
2021-12-11 12:43:23 +0100

    application starting point added, alongside with a complete pom.xml and an empty application.properties file

---
+++ src/main/java/hu/unideb/inf/webfejlesztesbeadando/Springboot_api.java
@@ -0,0 +1,12 @@
+package hu.unideb.inf.webfejlesztesbeadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot_api {
+
+	public static void main(String[] args) {
+		SpringApplication.run(hu.unideb.inf.webfejlesztesbeadando.Springboot_api.class, args);
+	}
+}
